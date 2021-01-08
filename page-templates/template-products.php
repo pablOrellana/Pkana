842a5b5dc65cfac1f8a1e3942d4c23628b448e8a
Pablo Orellana <pablo.orellana.r@uni.pe>
2021-01-08 10:15:04 -0500

    Agregar los templates de products y contact wp

---
+++ page-templates/template-products.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Productos
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+<?php
+get_footer();
