76b09e724dc49f045dc80dbb34bb9fe69edba792
Pablo Orellana <pablo.orellana.r@uni.pe>
2021-01-12 14:02:48 -0500

    Completar la pagina de contacto y formulario en wp

--- page-templates/template-contact.php
+++ page-templates/template-contact.php
@@ -11,6 +11,8 @@
 defined( 'ABSPATH' ) || exit;
 
 get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <section>
@@ -18,7 +20,26 @@ get_header();
     <div class="container">
       <div class="row">
         <div class="col-lg-5">
-          Consultas y cotizaciones
+          <div>
+            <?php the_title('<h2 class="entry-title">', '</h2>');?>
+          </div>
+          <div>
+          
+          </div>
+          <?php
+				    while ( have_posts() ) {
+				    	the_post();
+				    	get_template_part( 'loop-templates/content', 'page' );
+            
+				    	// If comments are open or we have at least one comment, load up the comment template.
+				    	if ( comments_open() || get_comments_number() ) {
+				    		comments_template();
+				    	}
+				    }
+				  ?>
+          <div>
+            <?php echo do_shortcode('[contact-form-7 id="19" title="Contact Form"]');?>
+          </div>
         </div>
         <div class="col-lg-7">
           <img src="https://picsum.photos/1200/1600" alt="Imagen de una Pkana">
